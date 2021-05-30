6a34b0f4daa2cb504e959994bafa556821fd7cdf
gunimus <regist@overspec.net>
2021-05-30 18:34:35 +0000

    Add timer text change

--- extension/timeKeeping.js
+++ extension/timeKeeping.js
@@ -18,7 +18,7 @@ initReplicant('GroupB');
 initReplicant('GroupC');
 
 function initReplicant(groupName) {
-    nodecg.Replicant("limit" + groupName).on("change", newValue => { limitTime[groupName] = newValue; });
+    nodecg.Replicant("data" + groupName).on("change", newValue => { limitTime[groupName] = newValue.limit_time; });
     currentTimeTextRep[groupName] = nodecg.Replicant("currentTimeText" + groupName);
     marginTopRep[groupName] = nodecg.Replicant("marginTop" + groupName);
     marginRightRep[groupName] = nodecg.Replicant("marginRight" + groupName);
@@ -27,30 +27,26 @@ function initReplicant(groupName) {
 }
 
 function start(selestGroup) {
-    nodecg.sendMessage("startButtonChange" + selestGroup, true);
-    nodecg.sendMessage("stopButtonChange" + selestGroup, false);
-    nodecg.sendMessage("resetButtonChange" + selestGroup, true);
+    buttonChange(selestGroup, true);
+    if (runningTimerRep[selestGroup].value != "Success") {
+        remainingTime[selestGroup] = getNowTime();
+        countDown[selestGroup] = setInterval(tryDecrement, 1000, selestGroup);
+    }
     runningTimerRep[selestGroup].value = "Start";
-    remainingTime[selestGroup] = getNowTime();
-    countDown[selestGroup] = setInterval(tryDecrement, 1000, selestGroup);
 }
 module.exports.start = start;
 
 function pause(selestGroup) {
-    nodecg.sendMessage("startButtonChange" + selestGroup, false);
-    nodecg.sendMessage("stopButtonChange" + selestGroup, true);
-    nodecg.sendMessage("resetButtonChange" + selestGroup, false);
-    runningTimerRep[selestGroup].value = "Pause";
+    buttonChange(selestGroup, false);
     if (typeof countDown[selestGroup] !== 'undefined') {
         clearInterval(countDown[selestGroup])
     }
+    runningTimerRep[selestGroup].value = "Pause";
 }
 module.exports.pause = pause;
 
 function reset(selestGroup) {
-    nodecg.sendMessage("startButtonChange" + selestGroup, false);
-    nodecg.sendMessage("stopButtonChange" + selestGroup, true);
-    runningTimerRep[selestGroup].value = "Stop";
+    buttonChange(selestGroup, false);
     if (typeof countDown[selestGroup] !== 'undefined') {
         clearInterval(countDown[selestGroup])
     }
@@ -58,24 +54,44 @@ function reset(selestGroup) {
     currentTimeTextRep[selestGroup].value = limitTime[selestGroup];
     marginTopRep[selestGroup].value = 58 + "px";
     marginRightRep[selestGroup].value = 0 + "px";
+    runningTimerRep[selestGroup].value = "Stop";
 }
 module.exports.reset = reset;
 
+function success(selestGroup) {
+    buttonChange(selestGroup, false);
+    currentTimeTextRep[selestGroup].value = "New PB!";
+    runningTimerRep[selestGroup].value = "Success";
+}
+module.exports.success = success;
+
 function tryDecrement(selestGroup) {
-    if (currentTime[selestGroup] != 0) {
+    if (currentTime[selestGroup] > 0) {
         let nowTime = getNowTime();
         let diffTime = nowTime - remainingTime[selestGroup];
         if (diffTime < 1000) {
             diffTime = 1000;
         }
         currentTime[selestGroup] = currentTime[selestGroup] - Math.floor(diffTime / 1000);
+        let marginTop = 691 - (633 * currentTime[selestGroup] / formatSeconds(limitTime[selestGroup]));
+        let marginRight = 571 - (571 * currentTime[selestGroup] / formatSeconds(limitTime[selestGroup]));
+        remainingTime[selestGroup] = getNowTime();
+        if (runningTimerRep[selestGroup].value != "Success") {
+            currentTimeTextRep[selestGroup].value = formatTime(currentTime[selestGroup]);
+            marginTopRep[selestGroup].value = marginTop + "px";
+            marginRightRep[selestGroup].value = marginRight + "px";
+        }
     }
-    currentTimeTextRep[selestGroup].value = formatTime(currentTime[selestGroup]);
-    let marginTop = 691 - (633 * currentTime[selestGroup] / formatSeconds(limitTime[selestGroup]));
-    marginTopRep[selestGroup].value = marginTop + "px";
-    let marginRight = 571 - (571 * currentTime[selestGroup] / formatSeconds(limitTime[selestGroup]));
-    marginRightRep[selestGroup].value = marginRight + "px";
-    remainingTime[selestGroup] = getNowTime();
+    if (currentTime[selestGroup] <= 0) {
+        buttonChange(selestGroup, false);
+        if (typeof countDown[selestGroup] !== 'undefined') {
+            clearInterval(countDown[selestGroup])
+        }
+        if (runningTimerRep[selestGroup].value != "Success") {
+            currentTimeTextRep[selestGroup].value = "Time up";
+            runningTimerRep[selestGroup].value = "Timeup";
+        }
+    }
 }
 
 function getNowTime() {
@@ -95,6 +111,14 @@ function formatSeconds(currentTime) {
     return Math.floor(timeParts[0] * 3600) + Math.floor(timeParts[1] * 60) + Math.floor(timeParts[2])
 }
 
+function buttonChange(selestGroup, runningTimer) {
+    nodecg.sendMessage("resetButtonChange" + selestGroup, runningTimer);
+    nodecg.sendMessage("startButtonChange" + selestGroup, runningTimer);
+    nodecg.sendMessage("stopButtonChange" + selestGroup, !runningTimer);
+    nodecg.sendMessage("successButtonChange" + selestGroup, !runningTimer);
+}
+
 nodecg.listenFor('startTimer', (newValue) => { start(newValue) });
 nodecg.listenFor('stopTimer', (newValue) => { pause(newValue) });
 nodecg.listenFor('resetTimer', (newValue) => { reset(newValue) });
+nodecg.listenFor('successTimer', (newValue) => { success(newValue) });
